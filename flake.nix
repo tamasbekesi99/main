3128c1955b7f0ed292273a32f80923859b91a979
tamasbekesi99 <bekesitommy@gmail.com>
2025-10-14 16:17:30 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Hyprland on Nixos";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    swww.url = "github:LGFae/swww";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.hyprland-btw = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.tommy = import ./home.nix;
+            backupFileExtension = "backup";
+          };
+        }
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,82 @@
+{ config, pkgs, ... }:
+
+let
+  dotfiles = "${config.home.homeDirectory}/nixos-dotfiles/config";
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+
+  configs = {
+    hypr = "hypr";
+    nvim = "nvim";
+    wofi = "wofi";
+    rofi = "rofi";
+    foot = "foot";
+    kitty = "kitty";
+    waybar = "waybar";
+    yazi = "yazi";
+  };
+in
+
+{
+
+imports =[
+  ./modules/theme.nix
+];
+
+  home.username = "tommy";
+  home.homeDirectory = "/home/tommy";
+  home.stateVersion = "25.05";
+  programs.git = {
+    enable = true;
+    userName = "tamasbekesi99";
+    userEmail = "bekesitommy@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";       
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos, btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#hyprland-btw";
+      ncg = "sudo nix-collect-garbage -d";
+      vim = "nvim";
+    };
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec uwsm start -S hyprland-uwsm.desktop
+      fi
+    '';
+  };
+
+ home.packages = with pkgs; [
+    neovim
+    ripgrep
+    nil
+    nixpkgs-fmt
+    nodejs
+    gcc
+    wofi
+    nitch
+    rofi
+    pcmanfm
+    (pkgs.writeShellApplication {
+      name = "ns";
+      runtimeInputs = with pkgs; [
+        fzf
+        (nix-search-tv.overrideAttrs {
+          env.GOEXPERIMENT = "jsonv2";
+        })
+      ];
+      text = ''exec "${pkgs.nix-search-tv.src}/nixpkgs.sh" "$@"'';
+    })
+  ];
+
+  xdg.configFile = builtins.mapAttrs
+    (name: subpath: {
+      source = create_symlink "${dotfiles}/${subpath}";
+      recursive = true;
+    })
+    configs;
+
+}
