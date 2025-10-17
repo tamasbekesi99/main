a754ace4e662ab0dde0d4d8b072408440ddaa3ac
tamasbekesi99 <bekesitommy@gmail.com>
2025-10-17 19:58:38 +0200

    made module for bash

---
+++ modules/bash.nix
@@ -0,0 +1,18 @@
+{config, pkgs, ...}:
+
+{
+programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos, btw";
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-dotfiles#hyprland-btw";
+      ncg = "sudo nix-collect-garbage -d";
+      vim = "nvim";
+    };
+    profileExtra = ''
+      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+        exec uwsm start -S hyprland-uwsm.desktop
+      fi
+    '';
+  };
+}
