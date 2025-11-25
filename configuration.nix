5744aa387678ed85b376aa89a9c65550df4d4667
tamasbekesi99 <bekesitommy@gmail.com>
2025-11-25 17:00:17 +0100

    switched to librewolf-bin

--- configuration.nix
+++ configuration.nix
@@ -94,12 +94,43 @@
     ];
   };
 
-  programs.firefox.enable = true;
+/*programs.firefox = {
+  enable = true;
+  package = pkgs.librewolf;
+  policies = {
+    DisableTelemetry = true;
+    DisableFirefoxStudies = true;
+    Preferences = {
+      "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
+      "cookiebanners.service.mode" = 2; # Block cookie banners
+      "privacy.donottrackheader.enabled" = true;
+      "privacy.fingerprintingProtection" = true;
+      "privacy.resistFingerprinting" = true;
+      "privacy.trackingprotection.emailtracking.enabled" = true;
+      "privacy.trackingprotection.enabled" = true;
+      "privacy.trackingprotection.fingerprinting.enabled" = true;
+      "privacy.trackingprotection.socialtracking.enabled" = true;
+    };
+    ExtensionSettings = {
+      "jid1-ZAdIEUB7XOzOJw@jetpack" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/duckduckgo-for-firefox/latest.xpi";
+        installation_mode = "force_installed";
+      };
+      "uBlock0@raymondhill.net" = {
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        installation_mode = "force_installed";
+      };
+    };
+  };
+};
+
+environment.etc."firefox/policies/policies.json".target = "librewolf/policies/policies.json";*/
+
 
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    librewolf
+    librewolf-bin
     foot
     brightnessctl
     btop
